9e7040bdcd6297eed04cf380cce11c6509a5fad5
Влад <mail.vchpro@yandex.ru>
2020-11-25 16:21:59 +0300

    bug fix

--- delprogress.php
+++ delprogress.php
@@ -7,13 +7,13 @@
 
     $data2 = $_GET;
 
-    if(isset ($data2["math"])) {
+    if(isset ($data2["math"]) && $user->math_level > 12) {
         $user->math_level = 1;
         R::store($user);
         echo "<script>document.location.href = '/math.php';</script>";
     }
 
-    if(isset ($data2["logic"])) {
+    if(isset ($data2["logic"]) && $user->logic_level > 16) {
         $user->logic_level = 1;
         R::store($user);
         echo "<script>document.location.href = '/logic.php';</script>";
